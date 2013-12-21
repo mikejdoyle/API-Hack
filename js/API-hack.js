0ea1cadf357785ca4a494aa9728337c3e577378e
Mike D <mike@buzzvertical.com>
2013-12-21 16:58:36 -0600

    Final Angular

--- js/API-hack.js
+++ js/API-hack.js
@@ -1,8 +1,9 @@
 //file:///Users/michaeldoyle/coding_projects/API-Hack/index.html
-var movies = [];
+function mctrl ($scope, $http){
+  $scope.minScore = 101;
+  $scope.movielist = [];
+  $scope.isError = false;
 
-function mctrl ($scope){
-  $scope.itemlist = [];
   $.ajax({
     url: 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json',
     data: {
@@ -11,24 +12,46 @@ function mctrl ($scope){
     type: 'GET',
     dataType: 'jsonp',
     success: function(data){
-        console.log('Success',data.movies);
-        $scope.movies = data.movies;
+        //console.log('Success',data.movies[0].title.replace(/\s/g, "") );
+        console.log('Success',data.movies, data.movies.length);
+        for(var i =0; i < data.movies.length ; i++){
+          $scope.movielist.push(data.movies[i]);
+        }
+        return $scope.movielist;
     },
     error: function(data){
         console.log('Error',data);
     }
-
-
-  /*$scope.clear = function(){
-          var oldList = $scope.itemlist;
-          $scope.itemlist = [];
-          angular.forEach (oldList, function (item){
-            if(!item.bought) $scope.itemlist.push(item);
-          });*/
   });
 
+  $scope.setMinScore = function(){
+    $scope.minScore = $scope.userMin;
+    $scope.isEmpty = true;
+  }
+
+   $scope.scoreMatch = function(  ) {
+    return function( item ) {
+      if (item.ratings.audience_score >= $scope.minScore){
+      $scope.isEmpty = false;
+      }
+      return item.ratings.audience_score >= $scope.minScore;
+
+    };
+  };
+
+  $scope.clear = function(){
+    $scope.minScore=101;
+    $scope.userMin="";
+    $scope.isEmpty = false;
+  }
+
+  $scope.audienceScore = function(item){
+    return Math.abs(99 - item.ratings.audience_score);
+  }
+
+
+};
 
 //Add ability for users to modify criteria
 //Once modified parse through movie list
 //Display new movie list
-};
